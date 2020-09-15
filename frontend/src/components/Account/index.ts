e975eb09b098c1329027d75cd3c0bed3f16c8eaf
Irena <irenejpopova6@gmail.com>
2020-09-15 11:36:06 +0200

    files added

--- frontend/src/components/Account/index.ts
+++ frontend/src/components/Account/index.ts
@@ -4,3 +4,11 @@ import { getUser } from "@actions/index";
 import { selectUser } from "@selectors/user";
 import { IState } from "@typings/state/index";
 import Account from "./Account";
+
+const mapStateToProps = (state: IState) => ({
+  user: selectUser(state),
+});
+
+const actions = { getUser };
+
+export default compose(connect(mapStateToProps, actions))(Account);
