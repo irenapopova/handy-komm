4e2a9b4d26455eb25cf3fa8f4100842063994ebc
Irena <irenejpopova6@gmail.com>
2020-09-16 19:58:27 +0200

    footer

---
+++ frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import "@styles/Footer.css";
+
+const Footer = () => (
+  <div className="footer">
+    <p>HandyKomm &copy; 2020</p>
+    <p>Impressum</p>
+  </div>
+);
+
+export default Footer;
